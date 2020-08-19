376ea188a0542f85e32900b6d1328407cc285e1e
Andrew Jensen <AJensen@student.neumont.edu>
2020-08-19 10:37:27 -0600

    Hook up database

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/data', {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connect('mongodb+srv://testUser:testUserPassword@cluster0.ktyj8.mongodb.net/data_express?retryWrites=true&w=majority', {useUnifiedTopology: true, useNewUrlParser: true});
 
 let mdb = mongoose.connection;
 
@@ -9,6 +9,18 @@ mdb.on('error', console.error.bind(console, 'connection error: '));
 mdb.once('open', callback => {
 
 });
+
+let accountSchema = mongoose.Schema({
+    username: String,
+    password: String,
+    email: String,
+    age: String,
+    answerOne: String,
+    answerTwo: String,
+    answerThree: String
+});
+
+let Account = mongoose.model('accounts', accountSchema);
 
 exports.index = (req, res) => {
     res.cookie('lastTimeVisited', Date.now(), {maxAge: 999999999999});
@@ -37,7 +49,20 @@ exports.signup = (req, res) => {
 }
 
 exports.signupPost = (req, res) => {
-    console.log(req.body.username);
+    let account = new Account({
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email,
+        age: req.body.age,
+        answerOne: req.body.answerOne,
+        answerTwo: req.body.answerTwo,
+        answerThree: req.body.answerThree
+    });
+
+    account.save((err, account) => {
+        if(err) return console.error(err);
+        console.log(req.body.username + "'s account created")
+    });
 
     res.redirect('/');
 }
